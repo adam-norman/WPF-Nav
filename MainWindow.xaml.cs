1656cce55a4f80af0b044ca5892bdecb7119d63a
Mahmoud Nasr <ma7moud.asp@gmail.com>
2021-01-26 23:47:35 +0200

    WPF-Navigation bars

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatrialDesign
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPopupLogout_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnSideMenuOpen_Click(object sender, RoutedEventArgs e)
+        {
+            btnSideMenuOpen.Visibility = Visibility.Collapsed;
+            btnSideMenuClose.Visibility = Visibility.Visible;
+        }
+
+        private void btnSideMenuClose_Click(object sender, RoutedEventArgs e)
+        {
+            btnSideMenuOpen.Visibility = Visibility.Visible;
+            btnSideMenuClose.Visibility = Visibility.Collapsed;
+        }
+    }
+}
